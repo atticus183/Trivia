12020e92330852c157596c89bfe3d12d12ecc74d
Josh R <atticus183@gmail.com>
2021-07-04 11:09:04 -0500

    Initial commit

---
+++ Trivia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Trivia
+//
+//  Created by Josh R on 7/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
